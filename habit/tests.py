0623a7bfe5e2c1d15d127c8cea0403abff0440dc
max <javoprav@gmail.com>
2023-08-02 19:59:10 +0300

    add tests

--- habit/tests.py
+++ habit/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from rest_framework.test import APITestCase
 from rest_framework import status
+
+from habit.models import Habit
 from users.models import User, UserRoles
 
 
@@ -21,4 +23,20 @@ class  CourseTestCase(APITestCase):
         response = self.client.post('/api/token/', {"email": "user@user.com", "password": "123"})
         self.access_token = response.json().get('access')
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
-        self.test_model_name = 'course_for_test'
+        self.test_model_name = 'habit_for_test'
+
+    def test_habit_create(self):
+        """Тест создания модели Habit"""
+        habit_test = Habit.objects.create(name=self.test_model_name, place="home", time="17:53",
+                                          action="pump up the press test",
+                                          is_pleasurable=True, periodic=1, reward=None, execution_time="00:02",
+                                          public=True, owner=self.user, associated_habit=None)
+        response = self.client.post('/api/habits/', {'name': self.test_model_name, "place": "home", "time": "17:53",
+                                                     "action": "pump up the press test", "is_pleasurable": True,
+                                                     "periodic": 1, "reward": 'None', "execution_time": "00:02",
+                                                     "public": True, "owner": 1, 'associated_habit': None})
+        response_data = response.json()
+        print(response_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(habit_test.name, 'habit_for_test')
+
